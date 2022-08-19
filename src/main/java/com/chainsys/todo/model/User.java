102868cfe3886a7f464b366769821cfcd29237bf
Omkumar T R <101443243+OmkumarTR@users.noreply.github.com>
2022-08-19 17:03:31 +0530

    sonar errors

--- src/main/java/com/chainsys/todo/model/User.java
+++ src/main/java/com/chainsys/todo/model/User.java
@@ -15,7 +15,6 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
